752fd0ffdd22e08cf55603c7c4e7095f4ba28b08
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-28 16:34:41 -0700

    added schema file

--- models/owner.js
+++ models/owner.js
@@ -1,10 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
     var Owner = sequelize.define("Owner", {
         email: {
-            type: DataTypes.STRING, 
-            isEmail: true, 
-            notEmpty: true
+            type: DataTypes.STRING,
+            notEmpty: true,
+            validate: {
+                isEmail: true,
+
+            }
         }, 
+        password: {
+            type: DataTypes.STRING, 
+            notEmpty: true,
+            validate: {
+                len: [6] 
+            }
+        }, 
+        
 
     });  
 }
---
+++ schema.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE homeys_db; 
