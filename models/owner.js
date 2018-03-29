811cc95959eb0df043bd0b04e9b0447938811c26
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-28 17:08:43 -0700

    owner model complete, its checking for unique values for email and user_name

--- models/owner.js
+++ models/owner.js
@@ -1,10 +1,32 @@
 module.exports = function(sequelize, DataTypes) {
     var Owner = sequelize.define("Owner", {
         email: {
-            type: DataTypes.STRING, 
-            isEmail: true, 
-            notEmpty: true
+            type: DataTypes.STRING,
+            validate: {
+              isEmail:true
+            },
+            unique: {
+                args: true,
+                msg: 'Email address already in use!'
+            }
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            notNull: true,
+            validate: {
+                len: [1, 100]
+            },
+            unique: {
+                args: true,
+                msg: 'Name is use!'
+            }
         }, 
-
-    });  
-}
+        pass: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [1, 100]
+            },
+        }, 
+    });
+    return Owner; 
+};
