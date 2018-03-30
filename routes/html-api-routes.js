220f31c123de3c114278a60529711ce0c12ddb52
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-30 10:46:21 -0700

    html-routes fixes

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -9,6 +9,10 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
   app.get("/owner/signup", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/owner.html"));
   });
