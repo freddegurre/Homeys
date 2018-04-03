dd2924a04dc0efe10bb94d64f5e6ab4bebbd8645
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-04-02 18:55:35 -0700

    lats/longs in place

--- schema.sql
+++ schema.sql
@@ -13,3 +13,7 @@ CREATE DATABASE homeys_db;
 
 --insert into properties (propName, streetAddress, zipCode, city, state, createdAt, updatedAt, ownerID)
 --values  ("Peach House", "105 Clarington Ave", "95614", "Davis", "CA", current_time(), current_time(), 2);
+
+
+insert into Providers (name, pass, zipCpde, dailyRate, createdAt, updatedAt)
+values ('Burt Reynolds', 1234567, 94545, 50, current_time(), current_time())
